a6995246b349333b65483808a94cefc1ea72d22f
Mmohamed33 <112021137+Mmohamed33@users.noreply.github.com>
2023-03-18 00:29:54 +0200

    version-1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $(".page-link1").click(function(){
+     $(".sec8").show();
+     $(".section88").hide();
+     $(".section888").hide();
+   
+   });
+
+   $(".page-link2").click(function(){
+     $(".sec88").show(); 
+     $(".sec8").hide();
+     $(".sec888").hide();
+    
+    });
+
+    $(".page-link3").click(function(){
+     $(".sec888").show(); 
+     $(".sec8").hide();
+     $(".sec88").hide();
+
+   });
+
+
+
+
+});
